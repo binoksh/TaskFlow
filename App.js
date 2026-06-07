a6aacb790cd9bacee93f5d218d44711d126ca994
biufer <167990583+binoksh@users.noreply.github.com>
2026-06-07 16:25:34 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Platform, StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { enableScreens } from 'react-native-screens';
+import AppNavigator from './navigation/AppNavigator';
+import { UserProvider } from './context/UserProvider';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+if (Platform.OS === 'web') {
+  enableScreens(false);
+}
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={styles.root}>
+      <SafeAreaProvider>
+        <UserProvider>
+          <NavigationContainer>
+            <AppNavigator />
+          </NavigationContainer>
+        </UserProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
